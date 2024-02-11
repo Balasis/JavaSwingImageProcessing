5980f7251e4327c3d4943c3b5da0b8149914c567
Vertimnus <iro.ikarevrev@gmail.com>
2024-02-11 17:06:10 +0200

    main imports added for reference

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.File;
+import javax.imageio.ImageIO;
 public class Main {
     public static void main(String[] args) {
         //read an image
