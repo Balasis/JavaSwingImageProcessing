9acff3a29a206298d67a5f49e1baf8031c0c8c62
Balasis <giovani1994a@gmail.com>
2024-02-12 17:06:31 +0200

    recommend

--- src/Main.java
+++ src/Main.java
@@ -5,86 +5,66 @@ import javax.imageio.ImageIO;
 public class Main {
         public static void main (String[]args){
             //There are extra info for each line above the method used on the line.(scroll below)
-            try{
-                //path to the img, We will change this later on how to choose the path
-                String imgPath="resources/theImage.png";
+            //path to the img, We will change this later on how to choose the path
+            String imgPath="resources/theImage.png";
 
+            BufferedImage currentImg;
+            int[][] currentImg2dArray;
+
+
+            try{
                 //we fetch the image from the path in order to create our BufferedImage obj
-                BufferedImage currentImg=fetchImage(imgPath);
+                 currentImg=fetchImage(imgPath);
 
                 //Creating a 2d Array using BufferedImage dimensions e.g (currentImg.getWidth() )
-                 int[][] currentImg2dArray= create2dArrayUsingBufferedImage(currentImg);
-
-                //Populate the currentImg2dArray with the corresponding RGB value of each pixel of currentImg
-                populate2dArrWithRGBFromTheBufferedImg(currentImg2dArray,currentImg);
-
-
-                //Turn the first  "x":30 in our case   rows rgb into black (0,0,0) (if rows exist)
-                turnFirstTenRowsIntoBlack(currentImg2dArray,130);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                //Let's recreate an image out of the current processed 2d Array
-
-
-                BufferedImage outputBufferedImage = new BufferedImage(currentImg2dArray.length, currentImg2dArray[0].length, BufferedImage.TYPE_INT_RGB);
-
-                for (int x = 0; x < currentImg2dArray.length; x++) {
-                    for (int y = 0; y < currentImg2dArray[0].length; y++) {
-                        outputBufferedImage.setRGB(x, y, currentImg2dArray[x][y]);
-                    }
-                }
-
-
-
-
-
-
-                File outputFile = new File("C:/Users/Oathkeeper/Desktop/output.png");
-                try {
-                    ImageIO.write(outputBufferedImage, "png", outputFile);
-                    System.out.println("Image saved successfully.");
-                } catch (IOException e) {
-                    System.err.println("Error saving image: " + e.getMessage());
-                }
-
-
-
-/*
-                                        //Testing functionality(Delete later):
-
-                //in order to see the real value from the int number that represent the rgb we need to do some
-                //shifting ( >> 24 ) and some masking  (0xFF)<-taking the last 8 LeastSignificantBits (LSB)
-                //The followings will be removed from this method its only for now for testing purposes
-                int alpha = (currentImg2dArray[0][0] >> 24) & 0xFF;
-                int red = (currentImg2dArray[0][0] >> 16) & 0xFF;
-                int green = (currentImg2dArray[0][0] >> 8) & 0xFF;
-                int blue = currentImg2dArray[0][0] & 0xFF;
-                System.out.println("Alpha: " + alpha);
-                System.out.println("Red: " + red);
-                System.out.println("Green: " + green);
-                System.out.println("Blue: " + blue);
-*/
-
-
-
+                currentImg2dArray=new create2dArrayUsingBufferedImage(currentImg);
             }
             catch (IOException e){
                 //printStackTrace returns a detailed list of exception including the method called during the exception(error)
                 e.printStackTrace();
             }
+
+
+
+
+
+
+            //Populate the currentImg2dArray with the corresponding RGB value of each pixel of currentImg
+            populate2dArrWithRGBFromTheBufferedImg(currentImg2dArray,currentImg);
+
+
+            //Turn the first  "x":30 in our case   rows rgb into black (0,0,0) (if rows exist)
+            turnFirstTenRowsIntoBlack(currentImg2dArray,130);
+
+
+
+            //Let's recreate an image out of the current processed 2d Array
+
+
+            BufferedImage outputBufferedImage = new BufferedImage(currentImg2dArray.length, currentImg2dArray[0].length, BufferedImage.TYPE_INT_RGB);
+
+            for (int x = 0; x < currentImg2dArray.length; x++) {
+                for (int y = 0; y < currentImg2dArray[0].length; y++) {
+                    outputBufferedImage.setRGB(x, y, currentImg2dArray[x][y]);
+                }
+            }
+
+
+
+
+            File outputFile = new File("C:/Users/Oathkeeper/Desktop/output.png");
+            try {
+                ImageIO.write(outputBufferedImage, "png", outputFile);
+                System.out.println("Image saved successfully.");
+            } catch (IOException e) {
+                System.err.println("Error saving image: " + e.getMessage());
+            }
+
+
+
+
+
+
 
         }
 
@@ -166,5 +146,29 @@ public class Main {
 
 
         }
+
+
+
+
+
+/*
+                                        //Testing functionality(Delete later):
+
+                //in order to see the real value from the int number that represent the rgb we need to do some
+                //shifting ( >> 24 ) and some masking  (0xFF)<-taking the last 8 LeastSignificantBits (LSB)
+                //The followings will be removed from this method its only for now for testing purposes
+                int alpha = (currentImg2dArray[0][0] >> 24) & 0xFF;
+                int red = (currentImg2dArray[0][0] >> 16) & 0xFF;
+                int green = (currentImg2dArray[0][0] >> 8) & 0xFF;
+                int blue = currentImg2dArray[0][0] & 0xFF;
+                System.out.println("Alpha: " + alpha);
+                System.out.println("Red: " + red);
+                System.out.println("Green: " + green);
+                System.out.println("Blue: " + blue);
+*/
+
+
+
+
 
 }
